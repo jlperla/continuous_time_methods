62e3cb153a902a69c54d6edcc8cfee99a4326dc8
jlperla <jesseperla@gmail.com>
2017-09-10 21:09:13 -0700

    Moved to a matlab location.  Test folder has direct download from HACT.

---
+++ continous_time_methods/optimal_stopping/matlab/tests/option_simple_LCP.m
@@ -0,0 +1,62 @@
+clear all; clc; close all;
+
+rho = 0.05;
+
+I= 1000;
+xmin = 0; xmax=1;
+x = linspace(xmin,xmax,I)';
+dx = x(2)-x(1);
+dx2 = dx^2;
+
+u = x.^(0.5);
+mu_bar = -0.01;
+mu = ones(I,1).*mu_bar;
+
+sig_bar = 0.01;
+sig = x.*sig_bar;
+sig2 = sig.^2;
+
+S = 10*ones(I,1);
+%S = 10*exp(0.05*x);
+
+
+%CONSTRUCT MATRIX
+X = - min(mu,0)/dx + sig2/(2*dx2);
+Y = - max(mu,0)/dx + min(mu,0)/dx - sig2/dx2;
+Z =  max(mu,0)/dx + sig2/(2*dx2);
+
+A =spdiags(Y,0,I,I)+spdiags(X(2:I),-1,I,I)+spdiags([0;Z(1:I-1)],1,I,I);
+A(I,I)= Y(I) + sig2(I)/(2*dx2); A(I,I-1) = X(I);
+
+%Solve option problem as LCP
+B = rho*speye(I) - A;
+q = -u + B*S; 
+
+%TRY DIFFERENT ALGORITHMS FOR SOLVING LCP
+tic
+disp('Solving LCP')
+
+%Test 1: Yuval Tassa's Newton-based LCP solver, download from http://www.mathworks.com/matlabcentral/fileexchange/20952
+z0 = zeros(I,1); l = zeros(I,1); u = Inf*ones(I,1);
+z = LCP(B,q,l,u,z0,1);
+
+%Test 2: Andreas Almqvist pivoting (Lemke) LCP solver, download from http://www.mathworks.com/matlabcentral/fileexchange/41485
+%[w,z,retcode] = LCPSolve(B,q);
+
+LCP_error = max(abs(z.*(B*z + q)));
+if LCP_error > 10^(-6)
+    disp('LCP not solved')
+    break
+end
+    
+V_LCP = z+S; %calculate value function
+toc
+
+error = z.*(B*z + q);
+plot(x,error)
+
+plot(x,V_LCP,x,S,'--','LineWidth',2)
+set(gca,'FontSize',16)
+legend('v(x)','S(x)','Location','NorthWest')
+xlabel('x')
+%print -depsc option_simple.eps
