377c5488cb1f49a0ec92469180e6e0156ebd27bb
stevenzhangdx <stevenzhangdx@gmail.com>
2017-09-12 10:52:08 -0700

    issue 5 complete

--- continous_time_methods/optimal_stopping/matlab/simple_optimal_stopping_diffusion.m
+++ continous_time_methods/optimal_stopping/matlab/simple_optimal_stopping_diffusion.m
@@ -33,15 +33,16 @@ function [results] = simple_optimal_stopping_diffusion(p, settings)
 	sigma_2 = sigma_2_x(x); %
 
 	%TODO: Explain the upwinding setup with these X, Y, Z terms.  Given matrices names to make them more clear and map to the code.
-	X = - min(mu,0)/dx + sigma_2/(2*dx_2);
-	Y = - max(mu,0)/dx + min(mu,0)/dx - sigma_2/dx_2;
-	Z =  max(mu,0)/dx + sigma_2/(2*dx_2);
+	X = - min(mu,0)/dx + sigma_2/(2*dx_2);               % equation (1)
+	Y = - max(mu,0)/dx + min(mu,0)/dx - sigma_2/dx_2;    % equation (2)
+	Z =  max(mu,0)/dx + sigma_2/(2*dx_2);                % equation (3)
 
 	%Creates the core finite difference scheme for the given mu and sigma_2 vectors.
-	A = spdiags(Y, 0, N_x, N_x) + spdiags(X(2:N_x),-1, N_x, N_x) + spdiags([0;Z(1:N_x-1)], 1, N_x, N_x);  
+	A = spdiags(Y, 0, N_x, N_x) + spdiags(X(2:N_x),-1, N_x, N_x) + spdiags([0;Z(1:N_x-1)], 1, N_x, N_x);    % equation (4)
 	%Term Y is on the diagonal except the last row. Every element below the diagonal is constructed by term X, and ones above the diagonal are contructed by term Z.
 	
 	A(N_x,N_x)= Y(N_x) + sigma_2(N_x)/(2*dx_2); A(N_x,N_x-1) = X(N_x); 
+    %The middle rows are represented as equation (5). In particular, the first row is written as equation (6) and the last row is equation (7).
 	%The last term on the diagonal becomes "- max(mu,0)/dx + min(mu,0)/dx - sigma_2/2*dx_2", which implements that v(x_max+epsilon)=x_max.
 	%TODO: Explain the left hand boundary value here?  Should we be making sure that v(x_min) <= S, for example... Verifying that S(x_min) > 0, for example would be enough?
 	%The first row of matrix A implements v(x_min-epsilon)=x_min.
@@ -52,19 +53,19 @@ function [results] = simple_optimal_stopping_diffusion(p, settings)
 	X_var1 = - mu/dx + sigma_2/(2*dx_2);
 	Y_var1 = mu/dx - sigma_2/dx_2;
 	Z_var1 = sigma_2/(2*dx_2);
-	A_var1 = spdiags(Y_var1, 0, N_x, N_x) + spdiags(X(2:N_x), -1, N_x, N_x) + spdiags([0;Z(1:N_x-1))], 1, N_x, N_x);
+	A_var1 = spdiags(Y_var1, 0, N_x, N_x) + spdiags(X(2:N_x), -1, N_x, N_x) + spdiags([0;Z(1:N_x-1)], 1, N_x, N_x);
 	A_var1(N_x,N_x)= Y(N_x) + sigma_2(N_x)/(2*dx_2);
 	% Variation 2: construct the A with a for loop, essentially adding in each row as an equation.  Map to exact formulas in a latex document.
 	S = zeros(N_x+2, N_x+2);
 	for i = 1: N_x
-	  x_i = -mu(i)/dx + sigma_2(i)/(2*dx_2);
-	  y_i = mu(i)/dx - sigma_2(i)/dx_2;
-	  z_i = sigma_2(i)/(2*dx_2);
+	  x_i = -mu(i)/dx + sigma_2(i)/(2*dx_2);  % equation (8)
+	  y_i = mu(i)/dx - sigma_2(i)/dx_2;       % equation (9)
+	  z_i = sigma_2(i)/(2*dx_2);              % equation (10)
 	  S(i+1, i) = x_i;
 	  S(i+1, i+1) = y_i;
 	  S(i+1, i+2) = z_i;
 	end
-	S(N_x+1, N_x+1) = mu(N_x)/dx - sigma_2(N_x)/(2*dx_2);
+	S(N_x+1, N_x+1) = mu(N_x)/dx - sigma_2(N_x)/(2*dx_2);  %% equation (11)
 	A_var2 = sparse(S(2: N_x+1, 2: N_x+1));
 	
 
