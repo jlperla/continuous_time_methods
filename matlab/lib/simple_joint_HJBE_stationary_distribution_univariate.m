212c8e69eb64a1924b5df94a80c1bb1be1a1695c
jlperla <jesseperla@gmail.com>
2017-09-18 12:59:19 -0700

    Added joint solution tests for sparse and dense.

---
+++ matlab/lib/simple_joint_HJBE_stationary_distribution_univariate.m
@@ -0,0 +1,79 @@
+%Takes the discretized operator A, the grid x, and finds the stationary distribution f.
+function [v, f, success] = simple_joint_HJBE_stationary_distribution_univariate(A, x, u, rho, settings)
+    I = length(x);
+    if nargin < 5
+       settings.default = true; %Just creates as required.
+    end
+
+    if(~isfield(settings, 'normalization'))
+       settings.normalization = 'sum'; %The only method supported right now is a direct sum
+       %Otherwise, could consider better quadrature methods using x such as trapezoidal or simpsons rule.
+    end
+    if ~isfield(settings, 'display')
+       settings.display = false; %Tolerance
+    end
+
+     if(isfield(settings, 'max_iterations')) %OTherwise use the default
+        max_iterations = settings.max_iterations; %Number of iterations
+     else
+        max_iterations = 10*I; %The default is too small for our needs
+     end
+     if(isfield(settings, 'tolerance')) %OTherwise use the default
+        tolerance = settings.tolerance; %Number of iterations
+     else
+        tolerance = []; %Empty tells it to use default
+     end 
+     if(~isfield(settings, 'preconditioner'))
+         settings.preconditioner = 'none'; %Default is no preconditioner.
+     end
+     if(~isfield(settings, 'sparse'))
+         settings.sparse = true;
+     end
+
+
+     if(isfield(settings, 'initial_guess'))         
+         initial_guess = settings.initial_guess;
+     else
+         initial_guess = [];
+     end
+     
+     
+    %Create the joint system
+    y = sparse([u; sparse(I,1);1]); %(42)
+    X = [(rho * speye(I) - A) sparse(I,I); sparse(I,I) A';sparse(1,I) ones(1,I)]; %(43).  Only supporting simple sum.
+
+    if(settings.sparse == true)
+         if(strcmp(settings.preconditioner,'jacobi'))
+             preconditioner = diag(diag(X)); %Jacobi preconditioner is easy to calculate.  Helps a little
+         elseif(strcmp(settings.preconditioner,'incomplete_LU'))
+             %Matter if it is negative or positive?
+             [L,U] = ilu(X(1:end-1,:))
+              preconditioner = L;
+         elseif(strcmp(settings.preconditioner,'none'))
+             preconditioner = [];
+         else
+             assert(false, 'unsupported preconditioner');
+         end
+
+        [val,flag,relres,iter] = lsqr(X, y, tolerance, max_iterations, preconditioner, [], initial_guess); %Linear least squares.  Note tolerance changes with I
+        if(flag==0)
+            success = true;
+            %Extracts the solution.
+            v = val(1:I);
+            f = val(I+1:end);
+        else
+            if(settings.display)
+                disp('Failure to converge: flag and residual');
+                [flag, relres]
+            end
+            success = false;
+            f = NaN;
+            v = NaN;
+        end
+    else %Otherwise solve as a dense system
+       val = full(X) \ full(y);
+       v = val(1:I);
+       f = val(I+1:end);
+       success = true;
+    end
+end	
--- matlab/tests/HJBE_discretized_univariate_test.m
+++ matlab/tests/HJBE_discretized_univariate_test.m
@@ -58,3 +58,71 @@ function bigger_value_function_test(testCase)
     verifyTrue(testCase,norm(v - v_check, Inf) < tolerances.test_tol, 'v value no longer matches');
     verifyTrue(testCase, success==true, 'unsuccesful');
 end
+% 
+%This will try to jointly solve for the KFE and the value function with linear least squares.
+function joint_value_function_KFE_test(testCase)
+    tolerances = testCase.TestData.tolerances;
+    
+    mu_x = @(x) -0.01 * ones(numel(x),1);
+    sigma_bar = 0.1;
+    sigma_2_x = @(x) (sigma_bar * ones(numel(x),1)).^2;
+    u_x = @(x) log(x);
+    rho = 0.05;
+    x_min = 0.1;
+    x_max = 2;
+    I = 1000;
+    x = linspace(x_min, x_max, I)';
+    A = discretize_univariate_diffusion(x, mu_x(x), sigma_2_x(x));     
+    u = u_x(x);
+    
+    %Solve the simple problem: rho v(x) = u(x) + A v(x) for the above process.
+    disp('Solving HJBE on its own');
+    tic;
+    [v, success] = simple_HJBE_discretized_univariate(A, x, u, rho);
+    toc;
+    %dlmwrite(strcat(mfilename, '_3_v_output.csv'), v, 'precision', tolerances.default_csv_precision); %Uncomment to save again
+    v_check = dlmread(strcat(mfilename, '_3_v_output.csv'));    
+    verifyTrue(testCase,norm(v - v_check, Inf) < tolerances.test_tol, 'v value no longer matches');
+    verifyTrue(testCase, success==true, 'unsuccesful');
+    
+    %Now find the KFE with linear least squares and the default preconditioner
+    settings.display = true;
+    %settings.method = 'LLS';
+    %settings.method = 'eigenproblem_all';
+    disp('Solving stationary distribution on its own');
+    tic;
+    f = stationary_distribution_discretized_univariate(A, x, settings);    
+    toc;
+    %dlmwrite(strcat(mfilename, '_3_f_output.csv'), f, 'precision', tolerances.default_csv_precision); %Uncomment to save again
+    f_check = dlmread(strcat(mfilename, '_3_f_output.csv'));    
+    verifyTrue(testCase,norm(f - f_check, Inf) < tolerances.test_tol, 'f value no longer matches');
+        
+    %Now, solve as a joint problem with LLS
+    %settings.preconditioner = 'none';
+    settings.sparse = 'false';
+    disp('Solving dense');
+    tic;
+    [v, f, success] = simple_joint_HJBE_stationary_distribution_univariate(A, x, u, rho, settings);
+    verifyTrue(testCase,norm(v - v_check, Inf) < tolerances.test_tol, 'v value no longer matches');        
+    verifyTrue(testCase,norm(f - f_check, Inf) < tolerances.test_tol, 'f value no longer matches');    
+    toc;
+
+    %Now try as a sparse system.
+    disp('Solving sparse with no preconditioner');
+    settings.sparse = 'true';   
+    tic;
+    [v, f, success] = simple_joint_HJBE_stationary_distribution_univariate(A, x, u, rho, settings);
+    toc;
+    verifyTrue(testCase,norm(v - v_check, Inf) < tolerances.test_tol, 'v value no longer matches');        
+    verifyTrue(testCase,norm(f - f_check, Inf) < tolerances.test_tol, 'f value no longer matches');    
+
+    %Now try as a sparse system with jacobi preconditioner
+    disp('Solving sparse with jacobi preconditioner');
+    settings.sparse = 'jacobi';    
+    tic;
+    [v, f, success] = simple_joint_HJBE_stationary_distribution_univariate(A, x, u, rho, settings);
+    toc;
+    verifyTrue(testCase,norm(v - v_check, Inf) < tolerances.test_tol, 'v value no longer matches');        
+    verifyTrue(testCase,norm(f - f_check, Inf) < tolerances.test_tol, 'f value no longer matches');        
+end
+
