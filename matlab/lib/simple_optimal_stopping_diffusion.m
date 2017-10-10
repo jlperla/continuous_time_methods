77629067cdf785f2ff04f48cf6405898dc80866a
stevenzhangdx <stevenzhangdx@gmail.com>
2017-10-10 11:42:38 -0700

    An attempt on solving LCP by using tomlab

--- matlab/lib/simple_optimal_stopping_diffusion.m
+++ matlab/lib/simple_optimal_stopping_diffusion.m
@@ -75,14 +75,14 @@ function [results] = simple_optimal_stopping_diffusion(p, settings)
         %Box bounds, z_L <= z <= z_U.  In this formulation this means 0 <= z_i < infinity
         z_L = zeros(I,1); %(12)
         z_U = inf(I,1);
-
+        
         z = LCP(B, q, z_L, z_U, z_iv, (settings.print_level > 0));
         error = z.*(B*z + q); %(11)
 
         LCP_error = max(abs(error));
         if(LCP_error > settings.error_tolerance)
             if(settings.display) 
-                disp('Failure to converge')
+                disp('Failure to converge under Yuval')
             end
             results.success = false;
             exit;
@@ -91,8 +91,73 @@ function [results] = simple_optimal_stopping_diffusion(p, settings)
         %% Convert from z back to v and plot results
         v = z + S; %(7) calculate value function, unravelling the "z = v - S" change of variables
     elseif strcmp(settings.method, 'knitro')
-        results = NaN;
-        assert(false, 'Knitro in progress');
+        % Convert the LCP problem to a QCP problem
+        % min z' * (B*z - q)
+        % s.t. z >= 0
+        %      B*z >= -q
+        
+        % Uses Knitro Tomlab solver
+        B = Delta * rho * speye(I) - A; %(6)
+        q = -u * Delta + B*S; %(8)
+        D = (Delta * rho * speye(I) - A);  % D is used for linear constraints, D == B
+        
+        z_iv = zeros(I,1); %initial guess.
+
+        %Box bounds, z_L <= z <= z_U.  In this formulation this means 0 <= z_i < infinity
+        z_L = zeros(I,1); %(12)
+        z_U = inf(I,1);
+        %B*z + q >= 0, b_L <= B*z <= b_U (i.e. -q_i <= (B*z)_i <= infinity)
+        b_L = -q;
+        b_U = inf(I,1);
+        
+        Name = 'simple_optimal_stopping_diffusion';
+        
+        mpec = zeros(I, 6);
+        mpec(:, 1) = linspace(1, I, I)';
+        mpec(:, 3) = linspace(1, I, I)';
+        mpec = sparse(mpec);
+        
+        Prob = qcpAssign(B, q, D, b_L, b_U, z_L, z_U, z_iv, mpec, Name);
+        
+        D = Prob.A;
+        
+        %D_orig = Prob.orgProb.A;  % Original matrix D
+        
+        % Enable some crossover iterations, to "polish" the solution a bit in
+        % case KNITRO chooses an interior point algorithm:
+
+        Prob.KNITRO.options.MAXCROSSIT = 100;
+        Prob.PriLevOpt = 0;   % No output return
+
+        % Solve the QP (with MPEC pairs) using KNITRO:
+        Result = tomRun('knitro',Prob,0);
+        
+        z = Result.x_k;
+        
+        % Values of slacks that were added by BuildMPEC
+        %slacks = z(I+1: 2*I);
+        
+        % Original D * original variables, subtract the constants in the
+        % constraints to get the result on the c(x) >= 0 form
+
+        dx = D(:,1:I) * z(1:I) - b_L;
+        
+        % Should be zero, or very close to zero:
+        error = dx(1: I) .* z(1: I);
+            
+        LCP_error = max(abs(error));
+        if(LCP_error > settings.error_tolerance)
+            if(settings.display) 
+                disp('Failure to converge under Knitro')
+            end
+            results.success = false;
+            exit;
+        end
+        
+        %% Convert from z back to v and plot results
+        v = z + S; %(7) calculate value function, unravelling the "z = v - S" change of variables
+%         results = NaN;
+%         assert(false, 'Knitro in progress');
     else
         results = NaN;
         assert(false, 'Unsupported method to solve the LCP');        
