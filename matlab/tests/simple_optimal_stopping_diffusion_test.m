f632205a1ad144782641163c3959743b1b46ec6f
jlperla <jesseperla@gmail.com>
2017-10-11 20:05:58 -0700

    All existing test cases turned on.

--- matlab/tests/simple_optimal_stopping_diffusion_test.m
+++ matlab/tests/simple_optimal_stopping_diffusion_test.m
@@ -387,127 +387,97 @@ function zero_mu_test(testCase)
     verifyTrue(testCase, max(abs(v - v_old)) < tolerances.test_tol, 'Value of solution no longer matches negative u(x) for small x example');
 end
 
-% function negative_mu_min_and_positive_mu_max_test(testCase)
-%     [settings, ~, tolerances] = unpack_setup(testCase);  
-%     
-%     %Rewriting parameters entirely.
-%     mu_bar = -0.01; %Drift.  Sign changes the upwind direction.
-%     sigma_bar = 0.01; %Variance
-%     S_bar = 10.0; %the value of stopping
-%     gamma = 0.5; %u(x) = x^gamma
-% 
-%     %Relevant functions for u(x), S(x), mu(x) and sigma(x) for a general diffusion dx_t = mu(x) dt + sigma(x) dW_t, for W_t brownian motion
-%     parameters.rho = 0.05; %Discount rate
-%     parameters.x_min = 0.1; %Reflecting barrier at x_min.  i.e. v'(x_min) = 0 as a boundary value
-%     parameters.x_max = 1.0; %Reflecting barrier at x_max.  i.e. v'(x_max) = 0 as a boundary value
-% 
-%     parameters.u_x = @(x) x.^gamma; %u(x) = x^0.5 in this example
-%     parameters.S_x = @(x) S_bar.*ones(numel(x),1); %S(x) = S_bar in this example
-%     parameters.mu_x = @(x) x - 0.5; %i.e. mu(x) = x - 0.5;
-%     parameters.sigma_2_x = @(x) (sigma_bar*x).^2; %i.e. sigma(x) = sigma_bar x
-%     
-%     %Create uniform grid and determine step sizes.
-%     results = simple_optimal_stopping_diffusion(parameters, settings);
-%     v = results.v;
-%     
-%     %dlmwrite(strcat(mfilename, '_11_v_output.csv'), results.v, 'precision', tolerances.default_csv_precision); %Uncomment to save again
-%     %Check all values
-%     v_old = dlmread(strcat(mfilename,'_11_v_output.csv')); %Loads old value, asserts identical.  Note that the precision of floating points in the .csv matters, and can't be lower than test_tol.
-%     verifyTrue(testCase, max(abs(v - v_old)) < tolerances.test_tol, 'Value of solution no longer matches negative u(x) for small x example');
-% end
-% 
-% 
-% 
-% function positive_mu_min_and_negative_mu_max_test(testCase)
-%     [settings, ~, tolerances] = unpack_setup(testCase);  
-%     
-%     %Rewriting parameters entirely.
-%     mu_bar = -0.01; %Drift.  Sign changes the upwind direction.
-%     sigma_bar = 0.01; %Variance
-%     S_bar = 10.0; %the value of stopping
-%     gamma = 0.5; %u(x) = x^gamma
-% 
-%     %Relevant functions for u(x), S(x), mu(x) and sigma(x) for a general diffusion dx_t = mu(x) dt + sigma(x) dW_t, for W_t brownian motion
-%     parameters.rho = 0.05; %Discount rate
-%     parameters.x_min = 0.1; %Reflecting barrier at x_min.  i.e. v'(x_min) = 0 as a boundary value
-%     parameters.x_max = 1.0; %Reflecting barrier at x_max.  i.e. v'(x_max) = 0 as a boundary value
-% 
-%     parameters.u_x = @(x) x.^gamma; %u(x) = x^0.5 in this example
-%     parameters.S_x = @(x) S_bar.*ones(numel(x),1); %S(x) = S_bar in this example
-%     parameters.mu_x = @(x) -x + 0.5; %i.e. mu(x) = -x + 0.5;
-%     parameters.sigma_2_x = @(x) (sigma_bar*x).^2; %i.e. sigma(x) = sigma_bar x
-%     
-%     %Create uniform grid and determine step sizes.
-%     results = simple_optimal_stopping_diffusion(parameters, settings);
-%     v = results.v;
-%     
-%     %dlmwrite(strcat(mfilename, '_12_v_output.csv'), results.v, 'precision', tolerances.default_csv_precision); %Uncomment to save again
-%     %Check all values
-%     v_old = dlmread(strcat(mfilename,'_12_v_output.csv')); %Loads old value, asserts identical.  Note that the precision of floating points in the .csv matters, and can't be lower than test_tol.
-%     verifyTrue(testCase, max(abs(v - v_old)) < tolerances.test_tol, 'Value of solution no longer matches negative u(x) for small x example');
-% end
+function negative_mu_min_and_positive_mu_max_test(testCase)
+    [settings, ~, tolerances] = unpack_setup(testCase);  
+    
+    %Rewriting parameters entirely.
+    sigma_bar = 0.01; %Variance
+    S_bar = 10.0; %the value of stopping
+    gamma = 0.5; %u(x) = x^gamma
+
+    %Relevant functions for u(x), S(x), mu(x) and sigma(x) for a general diffusion dx_t = mu(x) dt + sigma(x) dW_t, for W_t brownian motion
+    parameters.rho = 0.05; %Discount rate
+    parameters.x_min = 0.1; %Reflecting barrier at x_min.  i.e. v'(x_min) = 0 as a boundary value
+    parameters.x_max = 1.0; %Reflecting barrier at x_max.  i.e. v'(x_max) = 0 as a boundary value
+
+    parameters.u_x = @(x) x.^gamma; %u(x) = x^0.5 in this example
+    parameters.S_x = @(x) S_bar.*ones(numel(x),1); %S(x) = S_bar in this example
+    parameters.mu_x = @(x) .2 * (x - 0.5); %i.e. mu(x) = x - 0.5;
+    parameters.sigma_2_x = @(x) (sigma_bar*x).^2; %i.e. sigma(x) = sigma_bar x
+    
+    %Create uniform grid and determine step sizes.
+    results = simple_optimal_stopping_diffusion(parameters, settings);
+    v = results.v;
+    
+    %dlmwrite(strcat(mfilename, '_11_v_output.csv'), results.v, 'precision', tolerances.default_csv_precision); %Uncomment to save again
+    %Check all values
+    v_old = dlmread(strcat(mfilename,'_11_v_output.csv')); %Loads old value, asserts identical.  Note that the precision of floating points in the .csv matters, and can't be lower than test_tol.
+    plot(results.x, results.v, results.x, parameters.S_x(results.x));    
+    verifyTrue(testCase, max(abs(v - v_old)) < tolerances.test_tol, 'Value of solution no longer matches negative u(x) for small x example');
+end
 
 
-% function negative_mu_and_zero_sigma_test(testCase)
-%     [settings, ~, tolerances] = unpack_setup(testCase);  
-%     
-%     %Rewriting parameters entirely.
-%     mu_bar = -0.01; %Drift.  Sign changes the upwind direction.
-%     sigma_bar = 0; %Variance
-%     S_bar = 10.0; %the value of stopping
-%     gamma = 0.5; %u(x) = x^gamma
-% 
-%     %Relevant functions for u(x), S(x), mu(x) and sigma(x) for a general diffusion dx_t = mu(x) dt + sigma(x) dW_t, for W_t brownian motion
-%     parameters.rho = 0.05; %Discount rate
-%     parameters.x_min = 0.1; %Reflecting barrier at x_min.  i.e. v'(x_min) = 0 as a boundary value
-%     parameters.x_max = 1.0; %Reflecting barrier at x_max.  i.e. v'(x_max) = 0 as a boundary value
-% 
-%     parameters.u_x = @(x) x.^gamma; %u(x) = x^0.5 in this example
-%     parameters.S_x = @(x) S_bar.*ones(numel(x),1); %S(x) = S_bar in this example
-%     parameters.mu_x = @(x) mu_bar * ones(numel(x),1); %i.e. mu(x) = mu_bar
-%     parameters.sigma_2_x = @(x) sigma_bar * ones(numel(x),1); %i.e. sigma(x) = sigma_bar
-%     
-%     %Create uniform grid and determine step sizes.
-%     results = simple_optimal_stopping_diffusion(parameters, settings);
-%     v = results.v;
-%     
-%     %dlmwrite(strcat(mfilename, '_13_v_output.csv'), results.v, 'precision', tolerances.default_csv_precision); %Uncomment to save again
-%     %Check all values
-%     v_old = dlmread(strcat(mfilename,'_13_v_output.csv')); %Loads old value, asserts identical.  Note that the precision of floating points in the .csv matters, and can't be lower than test_tol.
-%     verifyTrue(testCase, max(abs(v - v_old)) < tolerances.test_tol, 'Value of solution no longer matches negative u(x) for small x example');
-% end
 
+function positive_mu_min_and_negative_mu_max_test(testCase)
+    [settings, ~, tolerances] = unpack_setup(testCase);  
+    %settings.error_tolerance = 1e-85; %unable to get a high level of accuracy.
+    settings.max_iter = 30000; %Needs more iterations for some reason.
+    %Rewriting parameters entirely.
+    sigma_bar = 0.01; %Variance
+    S_bar = 14.0; %the value of stopping
+    gamma = 0.5; %u(x) = x^gamma
 
-% function positive_mu_and_zero_sigma_test(testCase)
-%     [settings, ~, tolerances] = unpack_setup(testCase);  
-%     
-%     %Rewriting parameters entirely.
-%     mu_bar = 0.01; %Drift.  Sign changes the upwind direction.
-%     sigma_bar = 0; %Variance
-%     S_bar = 10.0; %the value of stopping
-%     gamma = 0.5; %u(x) = x^gamma
-% 
-%     %Relevant functions for u(x), S(x), mu(x) and sigma(x) for a general diffusion dx_t = mu(x) dt + sigma(x) dW_t, for W_t brownian motion
-%     parameters.rho = 0.05; %Discount rate
-%     parameters.x_min = 0.1; %Reflecting barrier at x_min.  i.e. v'(x_min) = 0 as a boundary value
-%     parameters.x_max = 1.0; %Reflecting barrier at x_max.  i.e. v'(x_max) = 0 as a boundary value
-% 
-%     parameters.u_x = @(x) x.^gamma; %u(x) = x^0.5 in this example
-%     parameters.S_x = @(x) S_bar.*ones(numel(x),1); %S(x) = S_bar in this example
-%     parameters.mu_x = @(x) mu_bar * ones(numel(x),1); %i.e. mu(x) = mu_bar
-%     parameters.sigma_2_x = @(x) sigma_bar * ones(numel(x),1); %i.e. sigma(x) = sigma_bar
-%     
-%     %Create uniform grid and determine step sizes.
-%     results = simple_optimal_stopping_diffusion(parameters, settings);
-%     v = results.v;
-%     
-%     %dlmwrite(strcat(mfilename, '_14_v_output.csv'), results.v, 'precision', tolerances.default_csv_precision); %Uncomment to save again
-%     %Check all values
-%     v_old = dlmread(strcat(mfilename,'_14_v_output.csv')); %Loads old value, asserts identical.  Note that the precision of floating points in the .csv matters, and can't be lower than test_tol.
-%     verifyTrue(testCase, max(abs(v - v_old)) < tolerances.test_tol, 'Value of solution no longer matches negative u(x) for small x example');
-% end
-%%
+    %Relevant functions for u(x), S(x), mu(x) and sigma(x) for a general diffusion dx_t = mu(x) dt + sigma(x) dW_t, for W_t brownian motion
+    parameters.rho = 0.05; %Discount rate
+    parameters.x_min = 0.1; %Reflecting barrier at x_min.  i.e. v'(x_min) = 0 as a boundary value
+    parameters.x_max = 1.0; %Reflecting barrier at x_max.  i.e. v'(x_max) = 0 as a boundary value
 
+    parameters.u_x = @(x) x.^gamma; %u(x) = x^0.5 in this example
+    parameters.S_x = @(x) S_bar.*ones(numel(x),1); %S(x) = S_bar in this example
+    parameters.mu_x = @(x) -x + 0.5; %i.e. mu(x) = -x + 0.5;
+    parameters.sigma_2_x = @(x) (sigma_bar*x).^2; %i.e. sigma(x) = sigma_bar x
+    
+    %Create uniform grid and determine step sizes.
+    settings.method='lemke'; %Works much better here, for some reason.
+    results = simple_optimal_stopping_diffusion(parameters, settings);
+    v = results.v;
+    
+    %dlmwrite(strcat(mfilename, '_12_v_output.csv'), results.v, 'precision', tolerances.default_csv_precision); %Uncomment to save again
+    %Check all values
+    v_old = dlmread(strcat(mfilename,'_12_v_output.csv')); %Loads old value, asserts identical.  Note that the precision of floating points in the .csv matters, and can't be lower than test_tol.
+    plot(results.x, results.v, results.x, parameters.S_x(results.x));    
+    verifyTrue(testCase, max(abs(v - v_old)) < tolerances.test_tol, 'Value of solution no longer matches negative u(x) for small x example');
+end
+
+function negative_mu_and_zero_sigma_test(testCase)
+    [settings, ~, tolerances] = unpack_setup(testCase);  
+    
+    %Rewriting parameters entirely.
+    mu_bar = -0.01; %Drift.  Sign changes the upwind direction.
+    sigma_bar = 0; %Variance
+    S_bar = 10.0; %the value of stopping
+    gamma = 0.5; %u(x) = x^gamma
+
+    %Relevant functions for u(x), S(x), mu(x) and sigma(x) for a general diffusion dx_t = mu(x) dt + sigma(x) dW_t, for W_t brownian motion
+    parameters.rho = 0.05; %Discount rate
+    parameters.x_min = 0.1; %Reflecting barrier at x_min.  i.e. v'(x_min) = 0 as a boundary value
+    parameters.x_max = 1.0; %Reflecting barrier at x_max.  i.e. v'(x_max) = 0 as a boundary value
+
+    parameters.u_x = @(x) x.^gamma; %u(x) = x^0.5 in this example
+    parameters.S_x = @(x) S_bar.*ones(numel(x),1); %S(x) = S_bar in this example
+    parameters.mu_x = @(x) mu_bar * ones(numel(x),1); %i.e. mu(x) = mu_bar
+    parameters.sigma_2_x = @(x) sigma_bar * ones(numel(x),1); %i.e. sigma(x) = sigma_bar
+    
+    %Create uniform grid and determine step sizes.
+    results = simple_optimal_stopping_diffusion(parameters, settings);
+    v = results.v;
+    
+    %dlmwrite(strcat(mfilename, '_13_v_output.csv'), results.v, 'precision', tolerances.default_csv_precision); %Uncomment to save again
+    %Check all values
+    v_old = dlmread(strcat(mfilename,'_13_v_output.csv')); %Loads old value, asserts identical.  Note that the precision of floating points in the .csv matters, and can't be lower than test_tol.
+    plot(results.x, results.v, results.x, parameters.S_x(results.x));    
+    verifyTrue(testCase, max(abs(v - v_old)) < tolerances.test_tol, 'Value of solution no longer matches negative u(x) for small x example');
+end
 
 %This test runs the test case with only the default parameters in settings.
 function default_parameters_test(testCase)
@@ -518,7 +488,7 @@ function default_parameters_test(testCase)
     
     %Create uniform grid and determine step sizes.
     results = simple_optimal_stopping_diffusion(parameters, settings);    
-    dlmwrite(strcat(mfilename,'_22_v_output.csv'), results.v, 'precision', tolerances.default_csv_precision); %To save results again
+    %dlmwrite(strcat(mfilename,'_22_v_output.csv'), results.v, 'precision', tolerances.default_csv_precision); %To save results again
     v_old = dlmread(strcat(mfilename,'_22_v_output.csv')); %Loads old value, asserts identical.  Note that the precision of floating points in the .csv matters, and can't be lower than test_tol.    
     verifyTrue(testCase, max(abs(results.v - v_old)) < tolerances.test_tol, 'Value of solution no longer matches default value');        
 end
