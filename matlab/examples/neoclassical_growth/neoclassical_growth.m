4a8e5e2d304b62d7e91141010d83712e1c3d844e
wangxy373 <32117078+wangxy373@users.noreply.github.com>
2017-11-10 00:14:51 -0800

    Update notation for mu

--- matlab/examples/neoclassical_growth/neoclassical_growth.m
+++ matlab/examples/neoclassical_growth/neoclassical_growth.m
@@ -11,7 +11,7 @@ gamma = 2;
 alpha = 0.3;
 delta = 0.05;
 rho = 0.05;
-Aprod = 1;
+Aprod = 1; %factor of production A 
 
 %calculate steady state level of capital 
 kss = (alpha*Aprod/(rho+delta))^(1/(1-alpha)); %(9) 
@@ -26,8 +26,8 @@ dk = (kmax-kmin)/(I-1);
 crit = 10^(-6);
 Delta = 1000;
 
-dVf = zeros(I,1);
-dVb = zeros(I,1);
+dV_F = zeros(I,1);
+dV_B = zeros(I,1);
 c = zeros(I,1);
 
 %INITIAL GUESS
@@ -38,36 +38,38 @@ maxit=10;
 for n=1:maxit
     V = v;
     % forward difference
-    dVf(1:I-1) = (V(2:I)-V(1:I-1))/dk; %(11)
-    dVf(I) = (Aprod.*kmax.^alpha - delta.*kmax)^(-gamma); %(13) state constraint, for stability
+    dV_F(1:I-1) = (V(2:I)-V(1:I-1))/dk; %(11)
+    dV_F(I) = (Aprod.*kmax.^alpha - delta.*kmax)^(-gamma); %(13) state constraint, for stability
     % backward difference
-    dVb(2:I) = (V(2:I)-V(1:I-1))/dk; %(12)
-    dVb(1) = (Aprod.*kmin.^alpha - delta.*kmin)^(-gamma); %(14) state constraint, for stability
+    dV_B(2:I) = (V(2:I)-V(1:I-1))/dk; %(12)
+    dV_B(1) = (Aprod.*kmin.^alpha - delta.*kmin)^(-gamma); %(14) state constraint, for stability
         
     %consumption and savings with forward difference
-    cf = dVf.^(-1/gamma);
-    muf = Aprod.*k.^alpha - delta.*k - cf; %(17)
+    c_F = dV_F.^(-1/gamma);
+    mu_F = Aprod.*k.^alpha - delta.*k - c_F; %(17)
     %consumption and savings with backward difference
-    cb = dVb.^(-1/gamma);
-    mub = Aprod.*k.^alpha - delta.*k - cb; %(18)
+    c_B = dV_B.^(-1/gamma);
+    mu_B = Aprod.*k.^alpha - delta.*k - c_B; %(18)
     %consumption and derivative of value function at steady state
-    c0 = Aprod.*k.^alpha - delta.*k;
-    dV0 = c0.^(-gamma);
+    c_0 = Aprod.*k.^alpha - delta.*k;
+    dV_0 = c_0.^(-gamma);
     
     % dV_upwind makes a choice of forward or backward differences based on
     % the sign of the drift    
-    If = muf > 0; %below steady state
-    Ib = mub < 0; %above steady state
+    If = mu_F > 0; %below steady state
+    Ib = mu_B < 0; %above steady state
     I0 = (1-If-Ib); %at steady state
 
-    dV_Upwind = dVf.*If + dVb.*Ib + dV0.*I0; %(19) important to include third term
+    dV_Upwind = dV_F.*If + dV_B.*Ib + dV_0.*I0; %(19) important to include third term
     c = dV_Upwind.^(-1/gamma);
     u = c.^(1-gamma)/(1-gamma); %(24)
     
     %CONSTRUCT MATRIX
-    X = -min(mub,0)/dk;
-    Y = -max(muf,0)/dk + min(mub,0)/dk;
-    Z = max(muf,0)/dk;
+    mu_B_m = min(mu_B,0); %General notation of plus/minus.
+	mu_F_p = max(mu_F,0); 
+    X = -mu_B_m/dk;    
+    Y = -mu_F_p/dk + mu_B_m/dk;
+    Z = mu_F_p/dk;
     
     %full matrix: slower
     %     for i=2:I-1
@@ -78,7 +80,7 @@ for n=1:maxit
     %     A(1,1)=y(1); A(1,2) = z(1);
     %     A(I,I)=y(I); A(I,I-1) = x(I);
    
-    %sparse matrix: faster
+    % Construct sparse A matrix  
     A =spdiags(Y,0,I,I)+spdiags(X(2:I),-1,I,I)+spdiags([0;Z(1:I-1)],1,I,I);
     B = (rho + 1/Delta)*speye(I) - A;
     
