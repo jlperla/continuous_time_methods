86375de8f7d5128674a786b29fbc8d737dc16b22
wangxy373 <32117078+wangxy373@users.noreply.github.com>
2017-11-09 22:59:50 -0800

    Changed notation to match notes

--- matlab/examples/neoclassical_growth/neoclassical_growth.m
+++ matlab/examples/neoclassical_growth/neoclassical_growth.m
@@ -6,14 +6,17 @@ format compact
 
 tic;
 
-s = 2;
-a = 0.3;
-d = 0.05;
-r = 0.05;
+%parameters for neoclassical growth model 
+gamma = 2;
+alpha = 0.3;
+delta = 0.05;
+rho = 0.05;
 Aprod = 1;
 
-kss = (a*Aprod/(r+d))^(1/(1-a));
+%calculate steady state level of capital 
+kss = (alpha*Aprod/(rho+delta))^(1/(1-alpha)); %(9) 
 
+%set up uniform grid with I discrete points
 I=10000;
 kmin = 0.001*kss;
 kmax = 2*kss;
@@ -28,28 +31,28 @@ dVb = zeros(I,1);
 c = zeros(I,1);
 
 %INITIAL GUESS
-v0 = (Aprod.*k.^a).^(1-s)/(1-s)/r;
+v0 = (Aprod.*k.^alpha).^(1-gamma)/(1-gamma)/rho;
 v = v0;
 
 maxit=10;
 for n=1:maxit
     V = v;
     % forward difference
-    dVf(1:I-1) = (V(2:I)-V(1:I-1))/dk;
-    dVf(I) = (Aprod.*kmax.^a - d.*kmax)^(-s); %state constraint, for stability
+    dVf(1:I-1) = (V(2:I)-V(1:I-1))/dk; %(11)
+    dVf(I) = (Aprod.*kmax.^alpha - delta.*kmax)^(-gamma); %(13) state constraint, for stability
     % backward difference
-    dVb(2:I) = (V(2:I)-V(1:I-1))/dk;
-    dVb(1) = (Aprod.*kmin.^a - d.*kmin)^(-s); %state constraint, for stability
+    dVb(2:I) = (V(2:I)-V(1:I-1))/dk; %(12)
+    dVb(1) = (Aprod.*kmin.^alpha - delta.*kmin)^(-gamma); %(14) state constraint, for stability
         
     %consumption and savings with forward difference
-    cf = dVf.^(-1/s);
-    muf = Aprod.*k.^a - d.*k - cf;
+    cf = dVf.^(-1/gamma);
+    muf = Aprod.*k.^alpha - delta.*k - cf; %(17)
     %consumption and savings with backward difference
-    cb = dVb.^(-1/s);
-    mub = Aprod.*k.^a - d.*k - cb;
+    cb = dVb.^(-1/gamma);
+    mub = Aprod.*k.^alpha - delta.*k - cb; %(18)
     %consumption and derivative of value function at steady state
-    c0 = Aprod.*k.^a - d.*k;
-    dV0 = c0.^(-s);
+    c0 = Aprod.*k.^alpha - delta.*k;
+    dV0 = c0.^(-gamma);
     
     % dV_upwind makes a choice of forward or backward differences based on
     % the sign of the drift    
@@ -58,8 +61,8 @@ for n=1:maxit
     I0 = (1-If-Ib); %at steady state
 
     dV_Upwind = dVf.*If + dVb.*Ib + dV0.*I0; %(19) important to include third term
-    c = dV_Upwind.^(-1/s);
-    u = c.^(1-s)/(1-s);
+    c = dV_Upwind.^(-1/gamma);
+    u = c.^(1-gamma)/(1-gamma); %(24)
     
     %CONSTRUCT MATRIX
     X = -min(mub,0)/dk;
@@ -77,7 +80,7 @@ for n=1:maxit
    
     %sparse matrix: faster
     A =spdiags(Y,0,I,I)+spdiags(X(2:I),-1,I,I)+spdiags([0;Z(1:I-1)],1,I,I);
-    B = (r + 1/Delta)*speye(I) - A;
+    B = (rho + 1/Delta)*speye(I) - A;
     
     b = u + V/Delta;
     V = B\b; %SOLVE SYSTEM OF EQUATIONS
@@ -100,8 +103,8 @@ grid
 xlabel('Iteration')
 ylabel('||V^{n+1} - V^n||')
 
-kdot = Aprod.*k.^a - d.*k - c;
-Verr = c.^(1-s)/(1-s) + dV_Upwind.*kdot - r.*V;
+kdot = Aprod.*k.^alpha - delta.*k - c;
+Verr = c.^(1-gamma)/(1-gamma) + dV_Upwind.*kdot - rho.*V;
 
 set(gca,'FontSize',14)
 plot(k,Verr,'LineWidth',2)
