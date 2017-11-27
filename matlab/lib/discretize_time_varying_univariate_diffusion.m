2249dd5c937be11ecee2027bdaa1ef0690209f85
sevhou <33160514+sevhou@users.noreply.github.com>
2017-11-26 21:59:09 -0800

    Discretize_time_varying_diff #32
    Revised time varying diffusion for non-uniform grid, uniform grid is simply a generalization of non uniform one.

--- matlab/lib/discretize_time_varying_univariate_diffusion.m
+++ matlab/lib/discretize_time_varying_univariate_diffusion.m
@@ -6,6 +6,9 @@
 
 %Pass in the vector of the grid x, and the vectors of mu and sigma_2 at the nodes, and returns a sparse discretized operator.
 %The mu and sigma_2 are assumed to be already stacked correctly (i.e., keeping all time together).
+
+
+%This so far is the explicit time procedure (Nov.26)
 function [A, Delta_p, Delta_m, h_p, h_m] = discretize_time_varying_univariate_diffusion(t, x, mu, sigma_2, check_absorbing_states)
     if nargin < 4
         check_absorbing_states = true;
@@ -19,7 +22,44 @@ function [A, Delta_p, Delta_m, h_p, h_m] = discretize_time_varying_univariate_di
 	h_p = [diff(t)' (t(N)-t(N-1))]'; % (67) h_{+}
     h_m = [t(2)-t(1) diff(t)']'; % %(68) h{i, -}
     
-    %Use mu, sigma_2, etc. to calculate A    
-	A = NaN; %TODO: Calculate correctly
+    % stack delta's into R^NI
+    Delta_stack_p=repmat(Delta_p,N,1);
+    Delta_stack_m=repmat(Delta_m,N,1); 
+      
+   %% Construct sparse A matrix with non-uniform grid (uniform case is just a generalization of non-uniform)
+    %For non-uniform grid, \Delta_{i, +}=x_{i+1} - x_{i} and \Delta_{i, -}=x_{i} - x_{i-1}
+    mu_m = min(mu,0); %General notation of plus/minus.
+    mu_p = max(mu,0); 		
+    X = - mu_m./Delta_stack_m + sigma_2 ./(Delta_stack_m.*(Delta_stack_p + Delta_stack_m)); %(74)
+    Y = - mu_p./Delta_stack_p + mu_m./Delta_stack_m - sigma_2./(Delta_stack_p .* Delta_stack_m); % (75)
+    Z =  mu_p./Delta_stack_p + sigma_2 ./ (Delta_stack_p.*(Delta_stack_p + Delta_stack_m)); % (76)
+    
+    %Creates A^n matrices that's are n-specific(time grid specific)
+    
+    for n=1:N
+        indx = I*(n-1)+1;
+        Xn = X(indx:indx+I-1);
+        Yn = Y(indx:indx+I-1);
+        Zn = Z(indx:indx+I-1);
+        A_{n} = spdiags([[Xn(2:I); NaN] Yn [NaN; Zn(1:I - 1)]], [-1 0 1], I,I);% (77) for each time node indexed by n, A_n is different as mu_n changes. The procedure similar to that in time-invariant case   
+        A_{n}(1,1) = Yn(1) + Xn(1);%Reflecting barrier, top corner of (77)
+        A_{n}(I,I) = Yn(I) + Zn(I);%Reflecting barrier, bottom corner of (77)
+    end
+    
+    % Create D_{p,n} using explicit time steps and create A matrix
+    
+    A = zeros(N*I,N*I);
+    
+    for n=1:N
+        if n<N
+            D_p = 1/h_p(n) * eye(I);% explicit time step (79)
+            indx = I*(n-1)+1;
+            A(indx:indx+I-1,indx:indx+2*I-1)=[A_{n}-D_p D_p]; % Matrix A in (93)
+        else
+            D_p = 1/h_p(n) * eye(I);
+            indx = I*(n-1)+1;
+            A(indx:indx+I-1,indx:indx+I-1)=A_{n}-D_p; % right corner of (93)
+        end
+    end
 
 end	
